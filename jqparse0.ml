99d5e079d9bf4156ba82081b4df564f5bb7ea925
Chet Murthy <chetsky@gmail.com>
2021-03-31 15:50:26 -0700

    add to expression-parser

--- jqparse0.ml
+++ jqparse0.ml
@@ -28,11 +28,88 @@ EXTEND
     exp exp_eoi
     ;
 
-  exp: [
-      "simple" [
+    dict_pair:
+      [ [ id = LIDENT ; ":" ; e = exp -> (ExpString id, e)
+        | e1 = exp ; ":" ; e2 = exp -> (e1,e2)
+      ] ]
+    ;
+
+    exp: [
+      "|" LEFTA [
+        e1 = exp ; "|" ; e2 = exp -> ExpSeq e1 e2
+      ]
+    | "," LEFTA [
+        e1 = exp ; "," ; e2 = exp -> ExpConcat e1 e2
+      ]
+    | "//" RIGHTA [
+        e1 = exp ; "//" ; e2 = exp -> ExpAlt e1 e2
+      ]
+    | "=" NONA [
+        e1 = exp ; "=" ; e2 = exp -> ExpAssign e1 e2
+      | e1 = exp ; "|=" ; e2 = exp -> ExpAssignOr e1 e2
+      | e1 = exp ; "+=" ; e2 = exp -> ExpAssignAdd e1 e2
+      | e1 = exp ; "-=" ; e2 = exp -> ExpAssignSub e1 e2
+      | e1 = exp ; "*=" ; e2 = exp -> ExpAssignMul e1 e2
+      | e1 = exp ; "/=" ; e2 = exp -> ExpAssignDiv e1 e2
+      | e1 = exp ; "|=" ; e2 = exp -> ExpAssignUpd e1 e2
+      | e1 = exp ; "%=" ; e2 = exp -> ExpAssignMod e1 e2
+      | e1 = exp ; "//=" ; e2 = exp -> ExpAssignDes e1 e2
+      ]
+    | "or" NONA [
+        e1 = exp ; "or" ; e2 = exp -> ExpAnd e1 e2
+      ]
+    | "and" NONA [
+        e1 = exp ; "and" ; e2 = exp -> ExpAnd e1 e2
+      ]
+    | "==" NONA [
+        e1 = exp ; "==" ; e2 = exp -> ExpEq e1 e2
+      | e1 = exp ; "!=" ; e2 = exp -> ExpNe e1 e2
+      | e1 = exp ; "<" ; e2 = exp -> ExpLt e1 e2
+      | e1 = exp ; ">" ; e2 = exp -> ExpGt e1 e2
+      | e1 = exp ; "<=" ; e2 = exp -> ExpLe e1 e2
+      | e1 = exp ; ">=" ; e2 = exp -> ExpGe e1 e2
+      ]
+    | "+" LEFTA [
+        e1 = exp ; "+" ; e2 = exp -> ExpAdd e1 e2
+      | e1 = exp ; "-" ; e2 = exp -> ExpSub e1 e2
+      ]
+    | "*" LEFTA [
+        e1 = exp ; "*" ; e2 = exp -> ExpMul e1 e2
+      | e1 = exp ; "/" ; e2 = exp -> ExpDiv e1 e2
+      | e1 = exp ; "%" ; e2 = exp -> ExpMod e1 e2
+      ]
+    | "-" [
+        "-" ; e = exp -> ExpNeg e
+      ]
+    | "?//" NONA [
+        e1 = exp ; "?//" ; e2 = exp -> ExpDesAlt e1 e2
+      ]
+    | "." LEFTA [
+        e = exp ; "." ; f=LIDENT ->
+        match e with [
+          ExpDot -> failwith "..fldname is a syntax error"
+        | _ -> ExpField e f
+        ]
+                   | e=exp ; "?" -> ExpQuestion e
+                   | e = exp ; OPT "." ; "[" ; e2 = exp ; "]" -> ExpDeref e e2
+                   | e = exp ; "[" ; "]" -> ExpBrackets e
+                   | e = exp ; "[" ; e1 = exp ; ":" ; e2 = OPT exp ; "]" -> ExpSlice e e1 e2
+                   | n = INT -> ExpInt n
+                   | n = FLOAT -> ExpFloat n
+      ]
+    | "simple" [
         "." -> ExpDot
+      | "." ; f=LIDENT -> ExpDotField f
+      | "." ; f=STRING -> ExpDotField f
       | ".." -> ExpDotDot
       | "$" ; id = LIDENT -> ExpDataVar id
+      | "break" ; "$" ; l=LIDENT -> ExpBreak l
+      | s = STRING -> ExpString s
+      | "@" ; id = LIDENT -> ExpFormat id
+      | "(" ; e = exp ; ")" -> e
+      | "[" ; e = exp ; "]" -> ExpCollect e
+      | "[" ; "]" -> ExpArray
+      | "{" ; l = LIST0 dict_pair SEP "," ; "}" -> ExpDict l
     ] ]
   ;
   exp_eoi : [ [ e = exp ; EOI -> e ] ] ;
--- jqtypes.ml
+++ jqtypes.ml
@@ -1,5 +1,46 @@
 
 type exp =
     ExpDot
+  | ExpDotField of string
   | ExpDotDot
   | ExpDataVar of string
+  | ExpBreak of string
+  | ExpField of exp * string
+  | ExpQuestion of exp
+  | ExpString of string
+  | ExpInt of string
+  | ExpFloat of string
+  | ExpDeref of exp * exp
+  | ExpSlice of exp * exp * exp option
+  | ExpBrackets of exp
+  | ExpDict of (exp * exp) list
+  | ExpArray
+  | ExpCollect of exp
+  | ExpFormat of string
+  | ExpSeq of exp * exp
+  | ExpConcat of exp * exp
+  | ExpAlt of exp * exp
+  | ExpAssign of exp * exp
+  | ExpAssignOr of exp * exp
+  | ExpAssignAdd of exp * exp
+  | ExpAssignSub of exp * exp
+  | ExpAssignMul of exp * exp
+  | ExpAssignDiv of exp * exp
+  | ExpAssignUpd of exp * exp
+  | ExpAssignMod of exp * exp
+  | ExpAssignDes of exp * exp
+  | ExpAnd of exp * exp
+  | ExpOr of exp * exp
+  | ExpEq of exp * exp
+  | ExpNe of exp * exp
+  | ExpGt of exp * exp
+  | ExpLt of exp * exp
+  | ExpGe of exp * exp
+  | ExpLe of exp * exp
+  | ExpAdd of exp * exp
+  | ExpSub of exp * exp
+  | ExpMul of exp * exp
+  | ExpDiv of exp * exp
+  | ExpMod of exp * exp
+  | ExpNeg of exp
+  | ExpDesAlt of exp * exp
